be421324a08c044aa9c233972e0a3aa850c6b61f
alexandre <alexandre@inertiacreeps.net>
2024-04-25 18:32:32 +0200

    V2

--- stock.py
+++ stock.py
@@ -1,2 +1,32 @@
-def add(id,mdp):
-    print("test")
+import pickle
+
+def add(id, mdp):
+    print("")
+
+    file = open('save', 'wb')
+    sauvegarde = [
+                id,
+                mdp,
+            ]
+    pickle.dump(sauvegarde, file)
+    file.close()
+
+def connect(idr, mdpr):
+    try:
+        file = open('save', 'rb')
+        sauvegarde = pickle.load(file)
+        id = sauvegarde[0]
+        mdp = sauvegarde[1]
+        file.close()
+        compar(idr, mdpr,  id, mdp)
+    except:
+        pass
+
+def compar(idr, mdpr, id, mdp):
+    if "idr" == "id":
+        if "mdpr" == "mdp":
+            print("vous êtes connecter")
+        else:
+            print("le mot de passe n'est pas bon")
+    else:
+        print("l'identifiant n'est pas bon")
